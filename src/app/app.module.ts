871e5e763120592725870f48cbb09bda75b8eaa8
Can Tosun <cantosun@Cans-MacBook-Pro.local>
2021-11-11 14:46:38 -0800

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { EmployeesComponent } from './employees/employees.component';
+import { EmployeeDetailsComponent } from './employees/employee-details/employee-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    EmployeesComponent,
+    EmployeeDetailsComponent
   ],
   imports: [
     BrowserModule
