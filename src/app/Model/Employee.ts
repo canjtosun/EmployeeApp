b906d2974d25442fbf857ae5dfa6c70f65033b9d
Can Tosun <cantosun@Cans-MBP.attlocal.net>
2021-11-12 01:01:57 -0800

    xlsl  added, Employee class created based on xlsl

--- src/app/Model/Employee.ts
+++ src/app/Model/Employee.ts
@@ -1,3 +1,15 @@
+//var names based on employee_data.xlsx
+//10 field match with first row - 10 columns
 export class Employee {
+  first_name!: string;
+  last_name!: string;
+  company_name!: string;
+  address!: string;
+  city!: string;
+  county!: string;
+  postal!: string;
+  phone!: string;
+  email!: string;
+  web!: string;
 
 }
