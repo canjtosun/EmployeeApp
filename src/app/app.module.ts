237e07f0e51cdb7026c5cffefb4f1952caf2aff2
Brian Chiu <64235121+sudona@users.noreply.github.com>
2021-11-13 13:21:20 -0800

    added in search module
    added in the search module for searching

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,8 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatRippleModule } from '@angular/material/core'; 
 import { MatButtonModule } from '@angular/material/button';
 import { MatListModule } from '@angular/material/list';
-import { MatIconModule } from '@angular/material/icon';
+import { MatIconModule } from '@angular/material/icon'; 
+import { MatInputModule } from '@angular/material/input'; 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; 
 
 import { AppComponent } from './app.component';
@@ -40,6 +41,7 @@ const routes: Routes = [
     MatButtonModule,
     MatListModule,
     MatIconModule,
+    MatInputModule,
     BrowserAnimationsModule
   ],
   providers: [],
